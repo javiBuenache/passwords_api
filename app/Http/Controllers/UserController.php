c3f31bf8e73e75fc32b669e3fcafd0d768206e82
Javier <javierbuenache86@gmail.com>
2019-12-08 23:51:57 +0100

    create_function destroy_UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -118,6 +118,11 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+        $user->delete();  
+        
+        return response()->json([
+            "message" => "usuario borrado"
+        ], 201 );
     }
 }
