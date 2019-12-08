b3b10c9a546f38045f4cecba9fbdb3ddcf472629
Javier <javierbuenache86@gmail.com>
2019-12-08 23:36:11 +0100

    add_belongsTo_Password

--- app/Password.php
+++ app/Password.php
@@ -8,4 +8,9 @@ class Password extends Model
 {
     protected $table = 'passwords';
     protected $fillable = ['title','password','category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
