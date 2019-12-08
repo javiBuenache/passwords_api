06e9c7d05401a4dfac07aab71f3f4fc34fbaa125
Javier <javierbuenache86@gmail.com>
2019-12-08 22:41:10 +0100

    create_model_password

---
+++ app/Password.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Password extends Model
+{
+    protected $table = 'passwords';
+    protected $fillable = ['title','password','category_id'];
+}
