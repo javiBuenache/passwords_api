1744afa893eb600f24376976c3f25c8d9401e149
Javier <javierbuenache86@gmail.com>
2019-12-08 23:35:07 +0100

    add_hasMany_User

--- app/User.php
+++ app/User.php
@@ -8,4 +8,9 @@ class User extends Model
 {
     protected $table = 'users';
     protected $fillable = ['name','email','password'];
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
 }
