f3af380011a14a8cbb2ce816e3fb9f00669fb580
Javier <javierbuenache86@gmail.com>
2019-12-08 22:44:36 +0100

    function store_createUsers_UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,7 +34,11 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
     }
 
     /**
